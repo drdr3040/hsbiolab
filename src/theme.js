5acee62d26ba697607f7aeaab0265e645970851e
clrdl <163401793+clrdl@users.noreply.github.com>
2024-06-24 15:16:14 +0900

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+// src/themes/theme.js
+
+const theme = {
+    colors: {
+      mainBlue: '#1677FF',  
+      subBlue: '#F0F5FF',  // 연한 파란색 박스들
+      White: '#FFFFFF',  // 텍스트, 박스 배경
+      Black: '#000000',
+      backGray: '#FAFAFA',  // gray backgroud
+      MessageGray: '#F6F6F9', // (e.g. 수분을 기록해보세요, 3개의 약이 남았어요)
+      baseRed: '#FFF1F0', // e.g. '위험' 
+      baseYellow: '#FFF7E6', // e.g. '조심'
+      baseGreen: '#F6FFED', // e.g. '안전'
+      NavBar: '#8B8B91', // Navigation Bar Icons & Text
+      iconBlueCheck: '#ADC6FF', // Blue Check Icon
+      iconBlueMain: '#4096FF',  // Default Icon Colors
+      textBlue: '#597EF7', // selected date
+      textRed: '#FF4D4F', // e.g. '위험', 빨산색 느낌표 아이콘 Base Color
+      textOrange: '#FA8C16', // e.g. '조심', 노란색 느낌표 아이콘 Base Color
+      textGreen: '#1BD644', // e.g. '안전', 초록색 체크 아이콘 Base Color
+      textGray: '#B9B9BE', // e.g. '수분', '혈압'
+      textDarkGray: '#5D5D62', // e.g. '수분을 기록해보세요!', '총 수분량'
+    }
+}
