4ef039248e4d1bf35ed91546bb0c9fd961f392d6
drdr3040 <110738697+drdr3040@users.noreply.github.com>
2024-06-25 17:51:53 +0900

    파일 위치 이동

--- src/screens/MainInformationScreen.js
+++ src/screens/MainInformationScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ScrollView, View, StyleSheet, TouchableOpacity, Text, Dimensions } from 'react-native';
-import ChronicKidneyDiseaseSection from '../components/ChronicKidneyDiseaseSection';
+import ChronicKidneyDiseaseSection from '../components/MainInformationSection/ChronicKidneyDiseaseSection';
 import DialysisSection from '../components/DialysisSection';
 import MedicationSection from '../components/MedicationSection';
 import DietSection from '../components/DietSection';
