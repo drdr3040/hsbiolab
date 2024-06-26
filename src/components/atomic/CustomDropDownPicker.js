83c12ce30cf92b8879dc3636f808706325b6334b
drdr3040 <110738697+drdr3040@users.noreply.github.com>
2024-06-26 16:58:16 +0900

    로그인 프로필 components 완료

---
+++ src/components/atomic/CustomDropDownPicker.js
@@ -0,0 +1,22 @@
+// src/components/CustomDropDownPicker.js
+import React from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import styles from '../../styles/CustomDropDownPickerStyles';
+
+const CustomDropDownPicker = ({ open, value, items, setOpen, setValue, setItems }) => {
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      dropDownDirection="TOP"
+      zIndex={5000}
+      containerStyle={styles.dropdown}
+    />
+  );
+};
+
+export default CustomDropDownPicker;
---
+++ src/styles/CustomDropDownPickerStyles.js
@@ -0,0 +1,12 @@
+// src/styles/CustomDropDownPickerStyles.js
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  dropdown: {
+    width: '100%',
+  },
+});
+
+export default styles;
